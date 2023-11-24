506f8bf8bba9fc96af44c408a13ddc4e61fe0b30
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2023-11-24 11:07:50 +0200

    save 3

--- js/task-02.js
+++ js/task-02.js
@@ -9,9 +9,13 @@ const ingredients = [
 
 const ul = document.getElementById("ingredients");
 
-ingredients.forEach(ingredient => {
+const liArray = ingredients.map(ingredient => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
+  return li;
+});
+
+liArray.forEach(li => {
   ul.appendChild(li);
 });
--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,20 @@
-
 const form = document.querySelector('.login-form');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const emailInput = form.elements.email;
-  const passwordInput = form.elements.password;
-
-  if (emailInput.value === '' || passwordInput.value === '') {
-    alert('all fields must be filled.');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+  
+  const { email, password } = form.elements; 
+  
+  if (email.value === '' || password.value === '') {
+    alert('all fields must be filled'); 
     return;
   }
-
+  
   const loginData = {
-    email: emailInput.value,
-    password: passwordInput.value
-  };
-
-  console.log(loginData);
-
-  form.reset();
+    email: email.value,
+    password: password.value
+  }; 
+  
+  console.log(loginData); 
+  form.reset(); 
 });
